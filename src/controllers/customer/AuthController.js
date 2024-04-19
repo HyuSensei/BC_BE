7c6a7dbef9a36c8fc0d7474045dad8baffa7e339
HyuSensei <110617220+HyuSensei@users.noreply.github.com>
2024-04-19 14:22:20 +0700

    update domain clear cookies

--- src/controllers/customer/AuthController.js
+++ src/controllers/customer/AuthController.js
@@ -225,7 +225,7 @@ const handleAuth = async (req, res) => {
 
 const logout = (req, res) => {
   try {
-    res.clearCookie("jwt", {domain: "https://skinlele.vercel.app"});
+    res.clearCookie("jwt", {domain: "skinlele.vercel.app"});
     return res.status(200).json({
       success: true,
       message: "Đăng xuất thành công !",
